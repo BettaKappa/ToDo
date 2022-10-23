e41715f345c3b9bf3ebbb658b3dd0f36f20c03f6
HereWeGoAgain() <112721415+XeniaBX@users.noreply.github.com>
2022-10-23 21:27:02 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace ToD0
         static void Main()
         {
             ForegroundColor = DarkCyan;
-            WriteLine("\r\n               ,--,                                                                       \r\n,-.----.    ,---.'|                             ,--.          ,--.                        \r\n\\    /  \\   |   | :       ,---,               ,--.'|        ,--.'|     ,---,. ,-.----.    \r\n|   :    \\  :   : |      '  .' \\          ,--,:  : |    ,--,:  : |   ,'  .' | \\    /  \\   \r\n|   |  .\\ : |   ' :     /  ;    '.     ,`--.'`|  ' : ,`--.'`|  ' : ,---.'   | ;   :    \\  \r\n.   :  |: | ;   ; '    :  :       \\    |   :  :  | | |   :  :  | | |   |   .' |   | .\\ :  \r\n|   |   \\ : '   | |__  :  |   /\\   \\   :   |   \\ | : :   |   \\ | : :   :  |-, .   : |: |  \r\n|   : .   / |   | :.'| |  :  ' ;.   :  |   : '  '; | |   : '  '; | :   |  ;/| |   |  \\ :  \r\n;   | |`-'  '   :    ; |  |  ;/  \\   \\ '   ' ;.    ; '   ' ;.    ; |   :   .' |   : .  /  \r\n|   | ;     |   |  ./  '  :  | \\  \\ ,' |   | | \\   | |   | | \\   | |   |  |-, ;   | |  \\  \r\n:   ' |     ;   : ;    |  |  '  '--'   '   : |  ; .' '   : |  ; .' '   :  ;/| |   | ;\\  \\ \r\n:   : :     |   ,/     |  :  :         |   | '`--'   |   | '`--'   |   |    \\ :   ' | \\.' \r\n|   | :     '---'      |  | ,'         '   : |       '   : |       |   :   .' :   : :-'   \r\n`---'.|                `--''           ;   |.'       ;   |.'       |   | ,'   |   |.'     \r\n  `---`                                '---'         '---'         `----'     `---'       \r\n                                                                                          \r\n");
+            WriteLine("\r\n>======>   >=>             >>       >==>    >=> >==>    >=> >=======> >======>     \r\n>=>    >=> >=>            >>=>      >> >=>  >=> >> >=>  >=> >=>       >=>    >=>   \r\n>=>    >=> >=>           >> >=>     >=> >=> >=> >=> >=> >=> >=>       >=>    >=>   \r\n>======>   >=>          >=>  >=>    >=>  >=>>=> >=>  >=>>=> >=====>   >> >==>      \r\n>=>        >=>         >=====>>=>   >=>   > >=> >=>   > >=> >=>       >=>  >=>     \r\n>=>        >=>        >=>      >=>  >=>    >>=> >=>    >>=> >=>       >=>    >=>   \r\n>=>        >=======> >=>        >=> >=>     >=> >=>     >=> >=======> >=>      >=> \r\n                                                                                   \r\n");
             WriteLine("\t" + "\t" + "\t" + "ДЛЯ ЗАПУСКА ПРОГРАММЫ НАЖМИТЕ ПРОБЕЛ");
             Arrow();
         }
@@ -31,7 +31,7 @@ namespace ToD0
 
             Note note3 = new()
             {
-                Name = "Сходить на пары",
+                Name = "Снова сходить на пары",
                 Description = "Не заснуть на БЖД",
                 EndDate = new DateTime(2022, 10, 16)
             };
@@ -59,7 +59,7 @@ namespace ToD0
                 note5
             };
 
-            DateTime dayNow = new (2022, 10, 15);
+            DateTime dayNow = new(2022, 10, 15);
             int position = 1;
             var key = ReadKey();
             while (key.Key != Enter)
@@ -78,9 +78,15 @@ namespace ToD0
                     case RightArrow:
                         dayNow = dayNow.AddDays(1);
                         break;
+                    case OemPlus:
+                        //allNotes = Add(position, dayNow, allNotes);
+                        break;
+                    case OemMinus:
+                        allNotes = Remove(position, dayNow, allNotes);
+                        break;
                     case Escape:
                         Clear();
-                        WriteLine("\r\n ____  _  _  ____ \r\n(  _ \\( \\/ )( ___)\r\n ) _ < \\  /  )__) \r\n(____/ (__) (____)\r\n");
+                        WriteLine("\r\n   ___   __   __   ___   \r\n  | _ )  \\ \\ / /  | __|  \r\n  | _ \\   \\ V /   | _|   \r\n  |___/   _|_|_   |___|  \r\n_|\"\"\"\"\"|_| \"\"\" |_|\"\"\"\"\"| \r\n\"`-0-0-'\"`-0-0-'\"`-0-0-' \r\n");
                         Environment.Exit(0);
                         break;
                 }
@@ -99,18 +105,18 @@ namespace ToD0
             ShowInfo(position, dayNow, allNotes);
             Arrow();
         }
-
         private static void ShowInfo(int position, DateTime date, List<Note> myNotes)
         {
             List<Note> sortedNotes = myNotes.Where(note => note.EndDate.Date == date.Date).ToList();
             WriteLine(sortedNotes[position - 1].Name +
-                      "\t" + "|" + "\t" + 
+                      "\t" + "|" + "\t" +
                       sortedNotes[position - 1].EndDate.Date + "\n" +
                       "------------------------------------------------" + "\n" + "\n" +
-                      " " + sortedNotes[position - 1].Description);
+                      " " + sortedNotes[position - 1].Description + "\n" + "\n" +
+                      "------------------------------------------------" + "\n" + "\n" +
+                      "Для возвращения меню нажмите Пробел");
         }
-
-        private static void ShowDate(DateTime date, List<Note> myNotes )
+        private static void ShowDate(DateTime date, List<Note> myNotes)
         {
             Clear();
             ForegroundColor = DarkCyan;
@@ -119,8 +125,30 @@ namespace ToD0
             List<Note> sortedNotes = myNotes.Where(note => note.EndDate.Date == date.Date).ToList();
             foreach (Note note in sortedNotes)
             {
-                    WriteLine("  " + note.Name);
+                WriteLine("  " + note.Name);
             }
-        }   
+        }
+        private static List<Note> Remove(int position, DateTime date, List<Note> myNotes)
+        {
+            List<Note> sortedNotes = myNotes.Where(note => note.EndDate.Date == date.Date).ToList();
+            Note forDelete = sortedNotes[position - 1];
+            myNotes.Remove(forDelete);
+            foreach (Note note in myNotes)
+            {
+                WriteLine("  " + note.Name);
+            }
+
+            return myNotes;
+
+        }
+
+        private static List<Note> Add(int position, DateTime date, List<Note> myNotes)
+        { 
+            List<Note> sortedNotes = myNotes.Where(note => note.EndDate.Date == date.Date).ToList();
+            Note newNote = new Note();
+            sortedNotes.Add(newNote);
+            
+            return myNotes;
+        }
     }
 }
